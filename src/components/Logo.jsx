b4233f6a1aa887a1a6b66680c5986aeed0b8a21c
VENKATASAI24 <venkatasai24@localhost.localdomain>
2024-05-23 19:16:56 +0530

    logo designed

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,26 +1,10 @@
+import logo from "../assets/apple-touch-icon.png";
+
 const Logo = () => {
   return (
-    <div className="flex items-center">
-      <svg
-        className="h-10 w-10 text-white"
-        viewBox="0 0 100 100"
-        fill="#2563eb"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <circle cx="50" cy="50" r="48" stroke="#a855f7" strokeWidth="4" />
-        <text
-          x="50%"
-          y="50%"
-          dominantBaseline="middle"
-          textAnchor="middle"
-          fontSize="50"
-          fill="white"
-          fontFamily="Nunito Sans"
-        >
-          UI
-        </text>
-      </svg>
-    </div>
+    <>
+      <img src={logo} height="40px" width="40px" alt="UI" />
+    </>
   );
 };
 
