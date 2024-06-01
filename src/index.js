f89890b88bc89d82c0f4b35fd01098c1533519e7
venkatasai24 <venkatasai24042004@gmail.com>
2024-06-01 07:43:37 +0530

    react dev tool disabled

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AuthProvider } from "./context/AuthProvider";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
